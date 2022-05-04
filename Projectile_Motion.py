d22e2632698d157e3e0ff9214bb297811db6a8c9
Yarib <yarib97@gmail.com>
2022-05-04 01:38:02 -0500

    Version 2.0

--- Projectile_Motion.py
+++ Projectile_Motion.py
@@ -2,51 +2,79 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import minimize
 import seaborn as sns
-# import matplotlib.patches as mpatches
 
-sns.set(palette='Pastel2')
-g = 9.81
-v_0 = float(input("Enter initial velocity: "))
-alpha = (float(input("Enter angle of launch: ")))*(np.pi/180)
-height = float(input("Enter initial height: "))
+# Accepting only numeric input from the user
+def check_numeric(value):
+    '''Checks if a string value is numeric and converts it to int or float'''
+    while True:
+        try:
+            value = int(value)
+            break
+        except ValueError:
+            try: 
+                value = float(value)
+                break
+            except ValueError:
+                value = input("Please, enter a numerical value: ")
+    return value
+ 
+# Receiving initial values from the user
+v_0 = check_numeric(input("Enter initial velocity [m/s]: "))
+angle_degrees = check_numeric(input("Enter angle of launch [deg]: "))
+angle = angle_degrees*(np.pi/180)
+height = check_numeric(input("Enter initial height [m]: "))
 
-def t_flight(v_0, alpha, height, g):
-    tf = (v_0*np.sin(alpha)+((v_0*np.sin(alpha))**2+2*g*height)**0.5)/g
+# Projectile motion functions
+def t_flight(v_0, angle, height, g=9.81):
+    '''Returns time of flight'''
+    tf = (v_0*np.sin(angle)+((v_0*np.sin(angle))**2+2*g*height)**0.5)/g
     return tf
 
-def h_range(v_0, alpha, height, g):
-    h = v_0*np.cos(alpha)*t_flight(v_0, alpha, height, g)
+def h_range(v_0, angle, height, g=9.81):
+    '''Returns horizontal range'''
+    h = v_0*np.cos(angle)*t_flight(v_0, angle, height, g)
     return h
 
-def y(x):
-    y = np.tan(alpha)*x-(g*(x**2))/(2*(v_0*np.cos(alpha))**2)+height
+def y(x, g=9.81):
+    '''Returns the height at any point of the motion'''
+    y = np.tan(angle)*x-(g*(x**2))/(2*(v_0*np.cos(angle))**2)+height
     return y
 
-t_f = t_flight(v_0, alpha, height, g)
-h_r = h_range(v_0, alpha, height, g)
-x = np.linspace(0, h_r, 100)
+time_of_flight = t_flight(v_0, angle, height)
+horizontal_range = h_range(v_0, angle, height)
+x = np.linspace(0, horizontal_range, 100)
 h = y(x)
 
 guess = np.array([1])
 max_x = minimize(lambda x: -y(x),guess)
 max_y = y(max_x.x[0]) 
-print(max_y)
 
-# Rectangle((x,y), width, height, color, facecolor, transparency level)
-# rect = mpatches.Rectangle((h_r*0.6,height), 1.6, 2, color = 'g', facecolor = 'g', alpha = 0.25)
-# plt.gca().add_patch(rect)
+# Set color palette
+sns.set_style('darkgrid')
 
-plt.figure(num=1, dpi=120)
+# Plotting figure
+plt.figure(num=1, dpi=120, figsize=(9,6))
 plt.plot(x,h,'b')
-plt.title('Projectile Motion', color = 'k', fontweight = 'bold', size = 16)
-plt.xlabel('Distance [m]', fontweight = 'bold', size = 11)
-plt.ylabel('Height [m]', fontweight = 'bold', size = 11)
-plt.plot(h_r,0, marker='D', color='r')
+plt.title('Projectile Motion',
+          color='k',
+          fontweight='bold',
+          fontname='Century Gothic',
+          size=16)
+plt.xlabel('Distance [m]',
+           fontweight='bold',
+           fontname='Century Gothic',
+           size=11)
+plt.ylabel('Height [m]',
+           fontweight='bold',
+           fontname='Century Gothic',
+           size=11)
+plt.plot(horizontal_range,0, marker='D', color='r')
 plt.plot(max_x.x[0],max_y, marker='D', color='g')
-# plt.legend(['Trajectory','Horizontal range', 'Max height'])
-
-plt.annotate(f'  {round(h_r,3)} [m]', xy = (h_r,0), xytext = (h_r,0))
-plt.annotate(f'Time of flight {round(t_f,2)} [s]', xy = (h_r*0.6,height))
-
-plt.grid()
+plt.annotate(f'Max range:\n {round(horizontal_range,3)} [m]',
+             xy=(horizontal_range,0),
+             xytext=(horizontal_range,0))
+plt.annotate(f'Time of flight:\n {round(time_of_flight,2)} [s]',
+             xy=(horizontal_range*0.6,height))
+plt.annotate(f'Max height:\n {round(max_y,3)} [m]',
+             xy=(horizontal_range*0.6,max_y))
 plt.show()
